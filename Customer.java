98db76a132f778dc5f5562339df65f7d12a7a835
Varlasha <95291773+Varlasha@users.noreply.github.com>
2022-03-21 18:14:50 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,27 +1,51 @@
 package sklad;
+
 import java.util.Random;
+
 public class Customer implements Runnable
+
 {
-    private Queue queue;
-    Customer(Queue queue)//Запуск потребителей
-    {
-    	this.queue = queue;
-        Thread customer=new Thread (this);
-        customer.start();
-    }
-    public void run()//Работа Потребителя
-    {
-        Random R=new Random();
-        try {
-            for (int i=0; i<9; i++){
-                int k=R.nextInt(queue.razmer()-1)+1;
-                for (int j=0; j<k; j++){
-                	queue.get();
-                }
-                Thread.sleep(200);
-            }
-        }
-        catch(Exception e){}
-    }
+
+private Queue queue;
+
+Customer(Queue queue)//Р—Р°РїСѓСЃРє РїРѕС‚СЂРµР±РёС‚РµР»РµР№
+
+{
+
+this.queue = queue;
+
+Thread customer=new Thread (this);
+
+customer.start();
+
 }
 
+public void run()//Р Р°Р±РѕС‚Р° РџРѕС‚СЂРµР±РёС‚РµР»СЏ
+
+{
+
+Random R=new Random();
+
+try {
+
+for (int i=0; i<9; i++){
+
+int k=R.nextInt(queue.razmer()-1)+1;
+
+for (int j=0; j<k; j++){
+
+queue.get();
+
+}
+
+Thread.sleep(200);
+
+}
+
+}
+
+catch(Exception e){}
+
+}
+
+}
