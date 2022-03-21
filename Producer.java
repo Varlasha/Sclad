68173c17fb4f785a3221c7ccf7180bd78b67264e
Varlasha <95291773+Varlasha@users.noreply.github.com>
2022-03-21 18:17:39 +0300

    Update Producer.java

--- Producer.java
+++ Producer.java
@@ -1,35 +1,65 @@
 package sklad;
+
 import java.util.Random;
+
 public class Producer implements Runnable
+
 {
-    private int pr=0;
-    private Queue queue;
-    Producer(Queue queue)//Запуск Производителя
-    {
-    	this.queue = queue;
-        Thread producer=new Thread(this);
-        producer.start();
-    }
-    public void run()//Работа Производителя
-    {
-        Random R=new Random();
-        try {
-            for (int i=0; i<9; i++)
-            {
-                if (queue.proverit())
-                {pr+=1;}
-                int k=R.nextInt(queue.razmer()-1)+1;
-                for (int j=0; j<k; j++)
-                {
-                	queue.putCheck(pr);
-                }
-                Thread.sleep(200);
-            }
 
-        }
-        catch(Exception e)
-        {
+private int pr=0;
 
-        }
-    }
+private Queue queue;
+
+Producer(Queue queue)//Р—Р°РїСѓСЃРє РџСЂРѕРёР·РІРѕРґРёС‚РµР»СЏ
+
+{
+
+this.queue = queue;
+
+Thread producer=new Thread(this);
+
+producer.start();
+
+}
+
+public void run()//Р Р°Р±РѕС‚Р° РџСЂРѕРёР·РІРѕРґРёС‚РµР»СЏ
+
+{
+
+Random R=new Random();
+
+try {
+
+for (int i=0; i<9; i++)
+
+{
+
+if (queue.proverit())
+
+{pr+=1;}
+
+int k=R.nextInt(queue.razmer()-1)+1;
+
+for (int j=0; j<k; j++)
+
+{
+
+queue.putCheck(pr);
+
+}
+
+Thread.sleep(200);
+
+}
+
+}
+
+catch(Exception e)
+
+{
+
+}
+
+}
+
 }
